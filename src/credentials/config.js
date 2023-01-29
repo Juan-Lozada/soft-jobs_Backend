41bb49ce9bd739b5066126fc6450f179fc815cec
Juan <juan.lozada1905@gmail.com>
2023-01-29 14:44:24 -0300

    cambio de puerto

--- src/credentials/config.js
+++ src/credentials/config.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg')
 require( 'dotenv' ).config()
-
  const credentials = {
         host: 'postgresql-jlserverdb.alwaysdata.net',
         user: 'jlserverdb',
@@ -10,7 +9,6 @@ require( 'dotenv' ).config()
         allowExitOnIdle: true
 
 }
-
 const pool = new Pool(credentials)
 
 module.exports = pool
